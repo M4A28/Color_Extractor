0be8bb8790c134a289f514c293b39326539c8747
Mohammed Mosa <68967059+M4A28@users.noreply.github.com>
2024-03-12 17:05:11 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+# Color Extractor
+This Python script extracts the most dominant colors from one or more images using the KMeans clustering algorithm. It saves the colors to a text file and a JSON file, and creates a color palette image for each input image.
+
+## Table of Contents
+- Installation
+- Usage
+- Output
+- License
+- Installation
+
+### The script requires the following Python libraries:
+
+- sklearn
+- numpy
+- argparse
+- csv
+- json
+- PIL (Pillow)
+- matplotlib
+You can install these with pip:
+```bash
+pip install scikit-learn numpy argparse csv json pillow matplotlib
+```
+
+### Usage
+You can run the script from the command line like this:
+
+```python
+python script.py -p image1.jpg image2.jpg -n 5 -o colors_palette
+```
+
+Replace 'image1.jpg' and 'image2.jpg' with the paths to your image files, and 5 with the number of colors you want to extract. The -o argument is optional; if you don’t provide it, the script will use 'colors_palette' as the default output file name.
+
+### Output
+The script will create a text file, a JSON file, and a color palette image for each input image. The text and JSON files contain the most dominant colors in hexadecimal format. The color palette image shows the colors as horizontal bars.
+
+| Image             |  Color palette |
+:-------------------------:|:-------------------------:
+![](output/1.jpg)  |  ![](output/1.jpg_color_palette.png)
+![](output/2.jpg)  |  ![](output/2.jpg_color_palette.png)
+![](output/3.jpg)  |  ![](output/3.jpg_color_palette.png)
+![](output/4.jpg)  |  ![](output/4.jpg_color_palette.png)
+![](output/5.jpg)  |  ![](output/5.jpg_color_palette.png)
+![](output/6.jpg)  |  ![](output/6.jpg_color_palette.png)
+
+### License
+This project is licensed under the terms of the MIT license.
+
+This README provides a brief overview of the script, instructions on how to install and use it, and information about the output. You can add more sections as needed, such as ‘Contributing’, ‘Credits’, and ‘Acknowledgments’. If you encounter any issues, please let me know! I’m here to help. 😊
+
---
+++ extract_color.py
@@ -0,0 +1,64 @@
+from sklearn.cluster import KMeans
+import numpy as np
+import argparse
+import csv
+import json 
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def extract_colors(image_path, num_colors):
+    image = Image.open(image_path)
+    image = image.resize((150, 150))  # reduce size to speed up processing
+    image_np = np.array(image)
+    flattened = image_np.reshape(-1, 3)
+    
+    kmeans = KMeans(n_clusters=num_colors)
+    labels = kmeans.fit_predict(flattened)
+    
+    colors = kmeans.cluster_centers_
+    return colors
+
+def rgb_to_hex(rgb):
+    return '#{:02x}{:02x}{:02x}'.format(int(rgb[0]), int(rgb[1]), int(rgb[2]))
+
+
+def plot_colors(colors, filename):
+    fig, ax = plt.subplots(1, 1, figsize=(12, 7.2), dpi=100)
+    plt.axis('off')
+    plt.imshow([colors.astype(int)], aspect='auto')
+    extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+    plt.savefig(filename, bbox_inches=extent)
+
+def save_colors_to_file(colors, txt_filename, json_filename):
+    hex_colors = [rgb_to_hex(color) for color in colors]
+
+    with open(txt_filename+".txt", 'w') as f:
+        for color in hex_colors:
+            f.write(color + '\n')
+
+    with open(json_filename+".json", 'w') as f:
+        json.dump(hex_colors, f)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Extract dominant colors from an image.')
+    parser.add_argument('-p', type=str, nargs='+', help='The path to the image file.')
+    parser.add_argument('-n', type=int, default=5, help='The number of colors to extract.')
+    parser.add_argument('-o', type=str, default='colors_palte', help='The path to  output file.')
+
+    args = parser.parse_args()
+
+    for image_path in args.p:
+        colors = extract_colors(image_path, args.n)
+        txt_filename = f'{image_path}_colors.txt'
+        json_filename = f'{image_path}_colors.json'
+        save_colors_to_file(colors, txt_filename, json_filename)
+        plot_filename = f'{image_path}_color_palette.png'
+        plot_colors(colors, plot_filename)
+        print(f"Color palette saved as '{plot_filename}'")
+        print(f"Colors saved as '{txt_filename}' and '{json_filename}'")
+
+
+if __name__ == '__main__':
+    main()
+
